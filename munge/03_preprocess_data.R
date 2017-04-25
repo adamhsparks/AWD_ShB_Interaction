bd7e7f3b09735397e51c13fc21ceaf31670d0cc9
adamhsparks <adamhsparks@gmail.com>
2017-04-26 07:05:34 +1000

    Calculate AUDPS for tiller sheath blight incidence
    Double check tiller sheath blight incidence calculations, there is a
    negative AUDPS value in 2015 data

--- munge/03_preprocess_data.R
+++ munge/03_preprocess_data.R
@@ -1,7 +1,45 @@
+# Add plot numbers just to keep track of the data for calculations -------------
+DS2015$PLOT <- rep(1:24, 2160)
+DS2016$PLOT <- rep(1:16, 1728)
+
+# calculate tiller sheath blight incidence AUDPDS ------------------------------
+
+#2015
+DATE_1_2015 <- DATE_2_2015 <- DS2015$DATE
+DATE_1_2015[which(DATE_1_2015 == min(DATE_1_2015))] = NA
+DATE_2_2015[which(DATE_2_2015 == max(DATE_2_2015))] = NA
+
+DATE_1 <- na.omit(DATE_1_2015)
+DATE_2 <- na.omit(DATE_2_2015)
+
+# using lubridate::difftime calculate the time between observations
+DAYS_2015 <- difftime(DATE_1, DATE_2)
+
+# Add DAYS column to DS2015
+DS2015$DAYS <-
+  c(rep(0, nrow(DS2015) - length(DAYS_2015)), DAYS_2015)
+
+# 2016
+DATE_1_2016 <- DATE_2_2016 <- DS2016$DATE
+DATE_1_2016[which(DATE_1_2016 == min(DATE_1_2016))] = NA
+DATE_2_2016[which(DATE_2_2016 == max(DATE_2_2016))] = NA
+
+DATE_1 <- na.omit(DATE_1_2016)
+DATE_2 <- na.omit(DATE_2_2016)
+
+# using lubridate::difftime calculate the time between observations
+DAYS_2016 <- difftime(DATE_1, DATE_2)
+
+# Add DAYS column to DS2015
+DS2016$DAYS <-
+  c(rep(0, nrow(DS2016) - length(DAYS_2016)), DAYS_2016)
+
 # Join the 2015 and 2016 Data into one Tibble ----------------------------------
 
 RAW_data <- as_tibble(rbind(as.data.frame(DS2015),
                             as.data.frame(DS2016)))
+
+# convert columns to factor
 
 RAW_data$YEAR <- as.factor(RAW_data$YEAR)
 RAW_data$ASMT <- as.factor(RAW_data$ASMT)
@@ -13,3 +51,10 @@ RAW_data$SMPL <- as.factor(RAW_data$SMPL)
 RAW_data$HILL <- as.factor(RAW_data$HILL)
 RAW_data$TIL <- as.factor(RAW_data$TIL)
 RAW_data$LEAF <- as.factor(RAW_data$LEAF)
+
+# calculate AUDPS --------------------------------------------------------------
+AUDPS <-
+  RAW_data %>%
+  group_by(YEAR, REP, TRT, WMGT, NRTE, PLOT) %>%
+  summarise_each(funs(audps(., dates = DAYS)),
+                 TShB_incidence)
