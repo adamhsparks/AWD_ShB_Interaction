dd6665276f7648348e7fff890ae3350bc6400575
Adam H. Sparks <adamhsparks@gmail.com>
2017-09-21 12:07:32 +1000

    Update authors and cleanup file structure
    - Delete "doc" directory
    
    - Add Nancy and Kyle as contributors to the package
    
    - Add rice.awd.pests-package documentation

---
+++ R/rice.awd.pests.R
@@ -0,0 +1,25 @@
+
+#' Reproducible Research Compendium for Analysing Effects of Water Management and Nitrogen on Rice Sheath Blight and Insect Populations
+#'
+#' @description
+#' Provides a research compendium of analysis associated with experiments
+#' conducted at the International Rice Research Institute (IRRI), Los Baños,
+#' Calabarzon, Philippines in the dry seasons of 2015 and 2016. The experiments
+#' were designed to test the effects of alternate wetting and drying irrigation
+#' management on rice sheath blight disease. The plots were split plot with an
+#' irrigation main plot and nitrogen sub-plot treatments.
+#'
+#' @name rice.awd.pests-package
+#' @author Adam H Sparks and Nancy P Castilla and Michael Noel
+#' @docType package
+#'
+#' @seealso
+#'\strong{Useful links:}
+#' \itemize{
+#' \item{\url{https://github.com/phytopathology/rice_awd_pests}}
+#' \item{Report bugs at
+#'  \url{https://github.com/phytopathology/rice_awd_pests/issues}}
+#' \item{Research compendium website
+#'  \url{https://phytopathology.github.io/rice_awd_pests/}}
+#' }
+NULL
