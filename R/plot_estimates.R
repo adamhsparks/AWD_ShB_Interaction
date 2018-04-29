cd678493fdf6f2604a94b98b6a8b60b847683841
Adam H. Sparks <adamhsparks@gmail.com>
2018-04-29 10:24:04 +1000

    Add plot_estimates()

---
+++ R/plot_estimates.R
@@ -0,0 +1,36 @@
+
+#' @title Plot MCMCglmm estimates
+#'
+#' @description Plot estimates of posterior distributions and credible
+#'  intervales of MCMCglmm outputs
+#'
+#' @name plot_estimates
+#' @param x An\code{\link[coda]{mcmc.list}} object
+#'
+#' @examples \dontrun{
+#' # Plot posterior means and credible intervals
+#' plot_estimates(m1)
+#'}
+#'
+#' @author Titus von der Malsburg, \email{malsburg@uni-potsdam.de}
+#'
+#' @export
+plot_estimates <- function(x) {
+  par(mfrow = c(1, 1))
+  if (class(x) != "summary.mcmc")
+    x <- summary(x)
+  n <- dim(x$statistics)[1]
+  par(mar = c(2, 7, 4, 1))
+  plot(
+    x$statistics[, 1],
+    n:1,
+    yaxt = "n",
+    ylab = "",
+    xlim = range(x$quantiles) * 1.2,
+    pch = 19,
+    main = "Posterior means and 95% credible intervals"
+  )
+  axis(2, at = n:1, rownames(x$statistics), las = 2)
+  arrows(x$quantiles[, 1], n:1, x$quantiles[, 5], n:1, code = 0)
+  abline(v = 0, lty = 2)
+}
