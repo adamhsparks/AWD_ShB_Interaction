834e440f8b565b922e556e382fe6a82c2f929108
Adam H. Sparks <adamhsparks@gmail.com>
2018-04-30 10:41:03 +1000

    Add plot_estimates_paper
    Add function to plot estimates for paper that does not include the intercept

--- R/plot_estimates_paper.R
+++ R/plot_estimates_paper.R
@@ -1,10 +1,10 @@
 
-#' @title Plot MCMCglmm estimates
+#' @title Plot MCMCglmm estimates for publication
 #'
 #' @description Plot estimates of posterior distributions and credible
-#'  intervals of MCMCglmm outputs
+#'  intervals of MCMCglmm outputs sans intercept for publication figure
 #'
-#' @name plot_estimates
+#' @name plot_estimates_paper
 #' @param x An\code{\link[coda]{mcmc.list}} object
 #'
 #' @examples \dontrun{
@@ -15,24 +15,26 @@
 #' @author Titus von der Malsburg, \email{malsburg@uni-potsdam.de}
 #'
 #' @export
-plot_estimates <- function(x) {
+plot_estimates_paper <- function(x) {
   par(mfrow = c(1, 1))
   if (class(x) != "summary.mcmc") {
     x <- summary(x)
   }
-  n <- dim(x$statistics)[1]
+  z <- x$statistics[-1, ]
+  zz <- x$quantiles[-1, ]
+  n <- dim(z)[1]
   par(mar = c(2, 7, 3, 1))
   plot(
-    x$statistics[, 1],
+    z[, 1],
     n:1,
     yaxt = "n",
     ylab = "",
     xlim = range(x$quantiles) * 1.2,
     pch = 19,
-    main = "Posterior means and 95% credible intervals",
+    main = "",
     bty = "L"
   )
-  axis(side = 2, at = n:1, rownames(x$statistics), las = 2)
-  arrows(x$quantiles[, 1], n:1, x$quantiles[, 5], n:1, code = 0)
+  axis(side = 2, at = n:1, rownames(z), las = 2)
+  arrows(zz[, 1], n:1, zz[, 5], n:1, code = 0)
   abline(v = 0, lty = 2)
 }
