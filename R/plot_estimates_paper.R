ee7e672f2398317d54192d9fd4e6003badc4d589
Adam H. Sparks <adamhsparks@gmail.com>
2018-04-30 22:36:39 +1000

    Add function to generate estimates and 95%CI for paper

--- R/plot_estimates_paper.R
+++ R/plot_estimates_paper.R
@@ -6,6 +6,7 @@
 #'
 #' @name plot_estimates_paper
 #' @param x An\code{\link[coda]{mcmc.list}} object
+#' @param main_title Main title for the plot
 #'
 #' @examples \dontrun{
 #' # Plot posterior means and credible intervals
@@ -15,26 +16,23 @@
 #' @author Titus von der Malsburg, \email{malsburg@uni-potsdam.de}
 #'
 #' @export
-plot_estimates_paper <- function(x) {
-  par(mfrow = c(1, 1))
+plot_estimates_paper <- function(x, main_title = "") {
   if (class(x) != "summary.mcmc") {
     x <- summary(x)
   }
-  z <- x$statistics[-1, ]
-  zz <- x$quantiles[-1, ]
-  n <- dim(z)[1]
-  par(mar = c(2, 7, 3, 1))
+  n <- dim(x$statistics)[1]
+  par(mar = c(2, 8, 3, 1))
   plot(
-    z[, 1],
+    x$statistics[, 1],
     n:1,
     yaxt = "n",
     ylab = "",
     xlim = range(x$quantiles) * 1.2,
     pch = 19,
-    main = "",
+    main = main_title,
     bty = "L"
   )
-  axis(side = 2, at = n:1, rownames(z), las = 2)
-  arrows(zz[, 1], n:1, zz[, 5], n:1, code = 0)
+  axis(side = 2, at = n:1, rownames(x$statistics), las = 2)
+  arrows(x$quantiles[, 1], n:1, x$quantiles[, 5], n:1, code = 0)
   abline(v = 0, lty = 2)
 }
