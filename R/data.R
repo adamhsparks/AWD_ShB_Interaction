1bbaddba72cb0539c9a7cbc83bb440b58068e44b
Adam H. Sparks <adamhsparks@gmail.com>
2020-06-12 14:27:46 +1000

    Remove data import from data-raw to vingettes
    Moves raw data to Zenodo and the Rmd into the vignettes where it downloads the data from Zenodo.
    
    Standardises author names across vignettes as well

--- R/data.R
+++ R/data.R
@@ -1,19 +1,20 @@
 
-#' A Tidy Data Frame of Area Under the Disease Progress Stairs for Sheath Blight Observations
+#' A Data Frame of Area Under the Disease Progress Curve and Stairs for Sheath Blight Observations
 #'
 #' A dataset containing the alternate wetting and drying study data observations
-#' and calculated Area Under the Disease Progress Stairs (AUDPS)
+#' and calculated Area Under the Disease Progress Curve (AUDPC) and Stairs
+#' (AUDPS)
 #'
-"AUDPS"
+"AUDPC_S"
 
-#' A Tidy Data frame of Raw Data for Sheath Blight Observations
+#' A Data frame of Raw Data for Sheath Blight Observations
 #'
 #' A dataset containing the alternate wetting and drying study data observations
 #' organised into a tidy data frame.
 #'
 "RAW_data"
 
-#' A data.frame of Nitrogen Rates and Split Application Timings
+#' A Data Frame of Nitrogen Rates and Split Application Timings
 #'
 #' A dataset containing the N rates and time of application split organised
 #' into a tidy data frame.
