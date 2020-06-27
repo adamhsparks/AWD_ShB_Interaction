dc8f63287e04cf2b959420b3e8d65c9c232ad54b
Adam H. Sparks <adamhsparks@gmail.com>
2020-06-27 14:27:30 +1000

    Clean up data preprocessing file and saved data files

--- R/data.R
+++ R/data.R
@@ -1,11 +1,10 @@
 
-#' A Data Frame of Area Under the Disease Progress Curve and Stairs for Sheath Blight Observations
+#' A Data Frame of Area Under the Disease Progress Stairs for Sheath Blight Observations
 #'
 #' A dataset containing the alternate wetting and drying study data observations
-#' and calculated Area Under the Disease Progress Curve (AUDPC) and Stairs
-#' (AUDPS)
+#' and calculated Area Under the Disease Progress Stairs (AUDPS)
 #'
-"AUDPC_S"
+"AUDPS"
 
 #' A Data frame of Raw Data for Sheath Blight Observations
 #'
